d4a6b6895730d6fbd41aebb4bb3cd3d302a7f95a
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-07-26 16:08:39 +0200

    linear y field

--- src/encodingfield.rs
+++ src/encodingfield.rs
@@ -35,7 +35,7 @@ impl EncodingField {
                     field.push(fov_x * (-0.5 + (x as f64) * step));
                 }
             }
-            (&SpatialDims::TwoD(nx, ny), &SpatialDims::TwoD(fov_x, fov_y)) => {
+            (&SpatialDims::TwoD(nx, ny), &SpatialDims::TwoD(fov_x, _)) => {
                 field = Vec::with_capacity(nx * ny);
                 let step = 1.0 - 1.0 / (nx as f64);
                 for _ in 0..ny {
@@ -44,7 +44,7 @@ impl EncodingField {
                     }
                 }
             }
-            (&SpatialDims::ThreeD(nx, ny, nz), &SpatialDims::ThreeD(fov_x, fov_y, fov_z)) => {
+            (&SpatialDims::ThreeD(nx, ny, nz), &SpatialDims::ThreeD(fov_x, _, _)) => {
                 field = Vec::with_capacity(nx * ny * nz);
                 let step = 1.0 - 1.0 / (nx as f64);
                 for _ in 0..nz {
@@ -66,16 +66,31 @@ impl EncodingField {
 
     pub fn linear_y(fov: SpatialDims<f64>, dimensions: SpatialDims<usize>) -> Self {
         let mut field: Vec<f64>;
-        match dimensions {
-            SpatialDims::OneD(nx) => {
-                field = Vec::with_capacity(nx);
+        match (&dimensions, &fov) {
+            (&SpatialDims::OneD(_), &SpatialDims::OneD(_)) => {
+                panic!("No y gradient in 1D problems");
             }
-            SpatialDims::TwoD(nx, ny) => {
+            (&SpatialDims::TwoD(nx, ny), &SpatialDims::TwoD(_, fov_y)) => {
                 field = Vec::with_capacity(nx * ny);
+                let step = 1.0 - 1.0 / (ny as f64);
+                for y in 0..ny {
+                    for _ in 0..nx {
+                        field.push(fov_y * (-0.5 + (y as f64) * step));
+                    }
+                }
             }
-            SpatialDims::ThreeD(nx, ny, nz) => {
+            (&SpatialDims::ThreeD(nx, ny, nz), &SpatialDims::ThreeD(_, fov_y, _)) => {
                 field = Vec::with_capacity(nx * ny * nz);
+                let step = 1.0 - 1.0 / (ny as f64);
+                for _ in 0..nz {
+                    for y in 0..ny {
+                        for _ in 0..nx {
+                            field.push(fov_y * (-0.5 + (y as f64) * step));
+                        }
+                    }
+                }
             }
+            _ => panic!("wrong"),
         }
         EncodingField {
             field,
